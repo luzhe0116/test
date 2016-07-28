1b007a568426225c1ffb8bf530cd977693aa55bd
单小飞 <2530101715@qq.com>
2016-07-28 17:06:05 +0800

    我添加了文件
    文件是用来记录行程流程的

---
+++ test/yongcheViewController.h
@@ -0,0 +1,14 @@
+//
+//  yongcheViewController.h
+//  test
+//
+//  Created by ekhome on 16/7/28.
+//  Copyright © 2016年 luzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface yongcheViewController : UIViewController
+/**           我这里是用来注释的            */
+/**           帮助很明了的读代码            */
+@end
---
+++ test/yongcheViewController.m
@@ -0,0 +1,37 @@
+//
+//  yongcheViewController.m
+//  test
+//
+//  Created by ekhome on 16/7/28.
+//  Copyright © 2016年 luzhe. All rights reserved.
+//
+
+#import "yongcheViewController.h"
+
+@interface yongcheViewController ()
+
+@end
+
+@implementation yongcheViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
