f1f709cbbce265ac8dfda7d53f3619c28c41a461
luzhe <luzhe0516@163.com>
2016-07-28 16:23:49 +0800

    12123
    看看

---
+++ test/2321ViewController.m
@@ -0,0 +1,38 @@
+//
+//  2321ViewController.m
+//  test
+//
+//  Created by 卢晨笑 on 16/7/28.
+//  Copyright © 2016年 luzhe. All rights reserved.
+//
+
+#import "2321ViewController.h"
+
+@interface _321ViewController ()
+
+@end
+
+@implementation _321ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"12435654374879697");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
